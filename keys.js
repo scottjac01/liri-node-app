00ed8668db8d3e3bf7e9fa428ab133315997d229
SAJ <27323862+scottjac01@users.noreply.github.com>
2017-08-28 02:54:32 -0400

    twitter omdb, and spotify working

--- keys.js
+++ keys.js
@@ -4,3 +4,8 @@ exports.twitterKeys = {
   access_token_key: "901451009897246720-zf9JCrMT7VVn4f8TZBPZStfLJB6awQx",
   access_token_secret: "Adwt5L7iZOUkobYl70uAmlxH97Ug5S9q6f0WgrIg5aEop"
 };
+
+exports.spotifyKeys = {
+	clientID: "2e9fc117f2a94c08b366718712712b4e",
+	clientSecret: "415ed058605e488d972eb477c48f0566"
+};
--- liri.js
+++ liri.js
@@ -7,37 +7,80 @@ var Twitter = require('twitter');
 //Requires inquirer
 var inquirer = require("inquirer");
 
+//Requires Spotify
+var Spotify = require('node-spotify-api');
+
 // Ask the question a series of questions
 inquirer.prompt([
 
   {
-    type: "input",
-    name: "name",
-    message: "Who are you???"
+    type: "list",
+    name: "itemPicked",
+    message: "What would you like to do???",
+    choices: ["my-tweets","spotify-this-song","movie-this","do-what-it-says"]
   },
+	{	
+		type: "input",
+	 	name: "songPicked",
+		message: "What song would you like to look up???"
+	}
+  ])
+.then(function(inquirerResponse) {
+    // inquirerResponse.
+    if (inquirerResponse.itemPicked === "my-tweets") {
+      var screenName = "scottjac01";
+	// Gets all of keys
+	var keys = authKeys.twitterKeys;
 
-// Gets all of keys
-var keys = authKey.twitterKeys;
+	// assigned twitterKeys and store in variables
+	var consumer_key = keys.consumer_key;
+	var consumer_secret = keys.consumer_secret;
+	var access_token_key = keys.access_token_key;
+	var access_token_secret = keys.access_token_secret;
 
-// assigned twitterKeys and store in variables
-var consumer_key = keys.consumer_key;
-var consumer_secret = keys.consumer_secret;
-var access_token_key = keys.access_token_key;
-var access_token_secret = keys.access_token_secret;
+	//create a new Twitter oobject called client
+	var client = new Twitter({
+	consumer_key: keys.consumer_key,
+	consumer_secret: keys.consumer_secret,
+	access_token_key: keys.access_token_key,
+	access_token_secret: keys.access_token_secret,
+	});
 
-//create a new Twitter oobject called client
-var client = new Twitter({
-consumer_key: keys.consumer_key,
-consumer_secret: keys.consumer_secret,
-access_token_key: keys.access_token_key,
-access_token_secret: keys.access_token_secret,
-});
-
-
-//call the twitter API
-client.get('statuses/user_timeline', { screen_name: 'nodejs', count: 20 }, function(error, tweets, response) {
-    if (!error) {
-		throw error;
-  console.log(tweets);  // The favorites. 
-  console.log(response);  // Raw response object. 
-
+	//call the twitter API
+	client.get('statuses/user_timeline', { screen_name: screenName , count: 20 }, function(error, tweets, response) {
+	    if (!error) {
+	    	for(var i = 0; i < tweets.length; i++){
+	    		console.log("==============================================");
+	  			console.log("Here are my tweets: \n" + tweets[i].user.name + "\n" + tweets[i].user.screen_name + "" +"\n" + tweets[i].user.friends_count + "\n" +
+	  			tweets[i].user.created_at + "\n" + tweets[i].text);
+	  			}
+				}
+				console.log(error);
+			});
+		}
+	///Next If statement
+	if (inquirerResponse.itemPicked === "spotify-this-song"){
+			    var songName = inquirerResponse.songPicked;
+			    var spotKeys = authKeys.spotifyKeys;
+					var spotify = new Spotify({
+	  				id: spotKeys.clientID,
+	  				secret: spotKeys.clientSecret
+					});
+	 
+					spotify.search({type: "track", query: songName, limit: 20 }, function(err, data) {
+  				if (err) {
+    				return console.log('Error occurred: ' + err);
+  					}
+	  				console.log(data.tracks.items[0].album.artists[0].name);
+	  					//for loop
+		    			for(var i = 0; i < data.tracks.items.length; i++){
+		    				console.log("==============================================");
+		    				console.log("Artist Name: " + data.tracks.items[i].album.artists[i].name + "\n" +
+		    					"Song Name: " + data.tracks.items[i].name + "\n" +
+		    					"Preview Link: " + data.tracks.items[i].album.external_urls.spotify + "\n" +
+		    					"Album Name: " + data.tracks.items[i].album.name);
+		    			}
+ 						console.log(data); 
+						});
+	  			}
+	  		});
